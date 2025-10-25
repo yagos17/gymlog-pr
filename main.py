0979636d8de97acf69a77e00c9de1063da6017cd
yagos17 <118472201+yagos17@users.noreply.github.com>
2025-10-25 13:03:44 -0300

    fix: Corrige erro persistente ao adicionar exercício

--- main.py
+++ main.py
@@ -3,29 +3,33 @@ from db import db
 from models import Exercicio
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///gymlog.db"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gymlog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+
     if request.method == 'POST':
-        nome_exercicio = request.form[home]
+        nome_exercicio = request.form['nome']
         grupo_muscular_exercicio = request.form['grupo_muscular']
+
         novo_exercicio = Exercicio(nome=nome_exercicio, grupo_muscular=grupo_muscular_exercicio)
 
-    try:
-        db.session.add(novo_exercicio)
-        db.session.commit()
-        return redirect(url_for('home'))
-    except Exception as e:
-        return f"Erro ao adicionar exercício: {e}"
+        try:
+            db.session.add(novo_exercicio)
+            db.session.commit()
+            return redirect(url_for('home'))
+        except Exception as e:
+            return f"Erro ao adicionar exercício: {e}"
+
     else:
-        exercicios = Exercicio.query.order_by(Exercicios.nome).all()
+        exercicios = Exercicio.query.order_by(Exercicio.nome).all()
         return render_template('exercicios.html', exercicios=exercicios)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-
     app.run(debug=True)
--- models.py
+++ models.py
@@ -4,8 +4,8 @@ class Exercicio(db.Model):
     __tablename__ = 'exercicios'
 
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), unique=True,nullable=False)
+    nome = db.Column(db.String(100), unique=True, nullable=False)
     grupo_muscular = db.Column(db.String(50), nullable=True)
 
     def __repr__(self):
-        return f"<Exercicio {self.nome}>"
+        return f'<Exercicio {self.nome}>'

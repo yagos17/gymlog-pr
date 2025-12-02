4f9994f0ebd59efb799aeec8128c3d7d3dc10ecb
yagos17 <yagolee70@gmail.com>
2025-12-01 22:00:40 -0300

    feat: Modelagem: Cria tabelas para Sessão e Séries de Treino

--- models.py
+++ models.py
@@ -9,3 +9,20 @@ class Exercicio(db.Model):
 
     def __repr__(self):
         return f'<Exercicio {self.nome}>'
+    
+class SessaoTreino(db.Model):
+    __tablename__ = 'sessao_treino'
+
+    id = db.Column(db.integer, primary_key=True)
+    data = db.Column(db.Date, nullable=False)
+    series = db.relationship('SerieExercicio', backref='sessao', lazy=True)
+
+class SerieExercicio(db.Model):
+    __tablename__ = 'series_exercicio'
+
+    id = db.Column(db.Integer, primary_key=True)
+    repeticoes = db.Column(db.Integer, nullable=False)
+    carga = db.Column(db.Float, nullable=True)
+    exercicio_id = db.Column(db.Integer, db.ForeignKey('exercicios.id'), nullable=False)
+    exercicio = db.relationship('Exercicio', backref='series')
+    sessao_id = db.Column(db.Integer, db.ForeignKey('sessao_treino.id'), nullable=False)

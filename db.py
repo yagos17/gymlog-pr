68d172954daf4cc21353632e2c297e5cc996a66c
yagos17 <yagolee70@gmail.com>
2025-12-03 17:12:57 -0300

    Update db.py

--- db.py
+++ db.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
+    if dbapi_connection.engine.url.drivername == "sqlite": 
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
